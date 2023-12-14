a0328ced065f69a776b4c723e001c9ee73ad1e4d
Ameer-eng <ameer@live.unc.edu>
2023-12-14 17:09:00 -0500

    Wrote initial report and edited makefile to generate the report

--- admission-rates-vs-test-scores.R
+++ admission-rates-vs-test-scores.R
@@ -13,10 +13,6 @@ institutionData$ADM_RATE_SUPP <- as.numeric(institutionData$ADM_RATE_SUPP)
 filteredData <- institutionData %>%
   filter(!is.na(SATVRMID) & !is.na(ACTCMMID) & !is.na(ADM_RATE_SUPP))
 
-# Histograms
-hist(filteredData$ACTCMMID)
-hist(filteredData$SATVRMID)
-
 # Scatter plots
 adm_rate_vs_sat <- ggplot(filteredData, aes(x = SATVRMID, y = ADM_RATE_SUPP)) +
   geom_point() +
